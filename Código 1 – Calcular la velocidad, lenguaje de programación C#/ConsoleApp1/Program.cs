6c4ec7977607a727765487ec8f2acd432f2ecb69
slayer0963 <landos112@gmail.com>
2018-03-20 16:17:02 -0600

    EJERCICIO 1
    EJERCICIO1

---
+++ Código 1 – Calcular la velocidad, lenguaje de programación C#/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d=0;
+            int t=0;
+            Console.WriteLine("Ingrese la distancia");
+            d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese la tiempo");
+            t = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("La velocidad es " + (d / t) +"k/h");
+            Console.ReadKey();
+
+        }
+    }
+}
